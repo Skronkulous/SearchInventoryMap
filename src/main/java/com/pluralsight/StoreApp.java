8e083516afae66f84d66efdf24462424e31c001a
Erik Holden <erikholden4@gmail.com>
2023-10-19 12:15:11 -0400

    Bug Fixes

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -29,34 +29,40 @@ public class StoreApp {
             br.close();
             menu();
         }
-        catch(Exception e){
+        catch(Exception fileError){
             System.out.println("There seems to be a problem with the current file path, please update and try again.");
-            e.printStackTrace();
+            fileError.printStackTrace();
         }
     }
     public static void searchItem(){
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("List of available key/IDs: ");
-        for(HashMap.Entry<Integer, Product> entry :inventoryList.entrySet()){
-            System.out.println("Key/ID: " + entry.getKey());
+        try{
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("List of available key/IDs: ");
+            for(HashMap.Entry<Integer, Product> entry :inventoryList.entrySet()){
+                System.out.println("Key/ID: " + entry.getKey());
+            }
+            System.out.print("Please enter the key/ID of the item you are interested in: ");
+            String choice = userInput.nextLine();
+            int temp = 0;
+            for(HashMap.Entry<Integer, Product> entry :inventoryList.entrySet()){
+                if(entry.getKey() == Integer.parseInt(choice)){
+                    System.out.println("\nKey/ID: " + entry.getKey() + " | Name: " + entry.getValue().getName() + " | Price: " + entry.getValue().getPrice());
+                    System.out.println();
+                    menu();
+                    break;
+                }
+                else if(temp == inventoryList.size()-1){
+                    System.out.println("\nWe could not find that Key/ID in our system, please try again!\n");
+                    searchItem();
+                }
+                else{
+                    temp++;
+                }
+            }
         }
-        System.out.print("Please enter the key/ID of the item you are interested in: ");
-        int choice = userInput.nextInt();
-        int temp = 0;
-        for(HashMap.Entry<Integer, Product> entry :inventoryList.entrySet()){
-            if(entry.getKey() == choice){
-                System.out.println("\nKey/ID: " + entry.getKey() + " | Name: " + entry.getValue().getName() + " | Price: " + entry.getValue().getPrice());
-                System.out.println();
-                menu();
-                break;
-            }
-            else if(temp == inventoryList.size()-1){
-                System.out.println("\nWe could not find that Key/ID in our system, please try again!\n");
-                searchItem();
-            }
-            else{
-                temp++;
-            }
+        catch (Exception searchError){
+            System.out.println("\nPlease enter a valid 4 digit Key/ID (Ex: 1234). \n");
+            searchItem();
         }
     }
     public static void menu(){
